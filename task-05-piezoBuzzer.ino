752cdbd957afd4215dbb95cb99a81201c8434721
Gunish <talk.gunish@gmail.com>
2020-07-08 19:22:50 +0530

    task 5

---
+++ task-05-piezoBuzzer.ino
@@ -0,0 +1,20 @@
+/*
+ * Task - 05
+ * Alarm sound using Piezo buzzer
+ */
+int buzzerPin = 8;
+unsigned int buzzerFrequency = 700;
+unsigned long buzzerDuration = 100;
+
+void setup() {
+
+}
+
+void loop() {
+  tone(buzzerPin, buzzerFrequency, buzzerDuration);
+  delay(500);
+  tone(buzzerPin, buzzerFrequency, buzzerDuration);
+  delay(1000);
+  noTone(buzzerPin);
+
+}
