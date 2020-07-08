1ce5f52b44775dc275f2e7198c856366930b1ff4
Gunish <talk.gunish@gmail.com>
2020-07-08 19:24:20 +0530

    task 7

---
+++ task-07-lm35TempSensor.ino
@@ -0,0 +1,25 @@
+/*
+ * Task -07
+ * Read LM35 Temperature sensor
+ */
+
+int tempSensorPin = A1;
+int tempSensorValue = 0;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  tempSensorValue = analogRead(tempSensorPin);
+  float mv = (tempSensorValue/1024.0)*5000;
+  float cel = mv/10;
+  float farh = (cel*9)/5 + 32;
+
+  Serial.print("Temperature : ");
+  Serial.print(cel);
+  Serial.print(" degree celsius");
+  Serial.print(farh);
+  Serial.print(" degree farenheit");
+  Serial.println();
+  delay(1000); 
+}
