9c3f9443d332ea33eb79d10f4aa205931ec4d05d
Gunish <talk.gunish@gmail.com>
2020-07-08 19:23:11 +0530

    task 6

---
+++ task-06-ldrLightSensor.ino
@@ -0,0 +1,16 @@
+/*
+ * Task -06
+ * Read LDR light sensor
+ */
+
+int lightSensorPin = A7;
+int lightSensorValue = 0;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  lightSensorValue = analogRead(lightSensorPin);
+  Serial.println(lightSensorValue);
+  delay(100);
+}
