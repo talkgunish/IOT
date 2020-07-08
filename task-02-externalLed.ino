6e910054c3cbc60f934c39972e9a6bff9162c9a9
Gunish <talk.gunish@gmail.com>
2020-07-08 19:20:42 +0530

    task 2

---
+++ task-02-externalLed.ino
@@ -0,0 +1,12 @@
+int ledOutputPin = 9;
+
+void setup() {
+  pinMode(ledOutputPin,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledOutputPin,HIGH);
+  delay(1000);
+  digitalWrite(ledOutputPin,LOW);
+  delay(1000);
+}
